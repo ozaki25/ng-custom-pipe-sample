6aaad1cff3360e15000e3dced4096b69ca4a75cf
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-02-05 19:09:31 +0900

    apply custom pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>{{ cardNumber | mask }}</h1>',
+  template: '<h1>{{ cardNumber | mask: 2 }}</h1>',
 })
 export class AppComponent {
   cardNumber = '1111-1111-1111-1111';
