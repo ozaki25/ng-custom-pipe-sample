455357722ca43a8cf55c7c9e25d37cf560c94c38
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-02-05 19:07:58 +0900

    update custom pipe

--- src/app/mask.pipe.ts
+++ src/app/mask.pipe.ts
@@ -1,12 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'mask'
-})
+@Pipe({ name: 'mask' })
 export class MaskPipe implements PipeTransform {
-
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: string): string {
+    return `${value.slice(0, -4)}****`;
   }
-
 }
