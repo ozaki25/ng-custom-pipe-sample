0af516abf48983eaac5dd20711f1e6e6de462969
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-02-05 19:10:10 +0900

    add custom pipe test

--- src/app/mask.pipe.spec.ts
+++ src/app/mask.pipe.spec.ts
@@ -1,8 +1,15 @@
 import { MaskPipe } from './mask.pipe';
 
 describe('MaskPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MaskPipe();
-    expect(pipe).toBeTruthy();
+  const pipe = new MaskPipe();
+  describe('文字数を指定しなかった場合', () => {
+    it('末尾4文字がマスク化されること', () => {
+      expect(pipe.transform('12345678')).toBe('1234****');
+    });
+  });
+  describe('文字数を指定した場合', () => {
+    it('末尾から指定した文字数分だけマスク化されること', () => {
+      expect(pipe.transform('12345678', 2)).toBe('123456**');
+    });
   });
 });
